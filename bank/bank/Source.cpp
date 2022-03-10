2e0444b344922de9ff5957672c03d182423bd9e8
Kris1100 <bobr.200@yandex.ru>
2022-03-10 22:10:07 +0300

    Add some changes

---
+++ bank/bank/Source.cpp
@@ -0,0 +1,13 @@
+#pragma once
+#include <iostream>
+#include <Windows.h>
+#include "PersonalBankAccount.h"
+
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "");
+	PersonalBankAccount t;
+	cin >> t;
+	return 0;
+}
