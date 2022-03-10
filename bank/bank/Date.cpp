38e37bdb843cccaaa6835514ae9432e439504031
Kris1100 <bobr.200@yandex.ru>
2022-03-10 18:40:44 +0300

    Add class Date

--- bank/bank/Date.cpp
+++ bank/bank/Date.cpp
@@ -1,1 +1,20 @@
+#define _CRT_SECURE_NO_WARNINGS //для работы localtime
 #include "Date.h"
+
+Date::Date() {
+	time_t now = time(0);
+	tm* ltm = localtime(&now);
+
+	set_day(ltm->tm_mday);
+	set_month(1 + ltm->tm_mon);
+	set_year(1900 + ltm->tm_year);
+	set_hour(ltm->tm_hour);
+	set_minute(ltm->tm_min);
+	set_second(ltm->tm_sec);
+}
+
+std::ostream& operator<<(ostream& out, Date& t) {
+	cout<< "Date: " << t.get_day() << "." << t.get_month() << "." << t.get_year() << endl;
+	cout << "Time: " << t.get_hour() << ":" << t.get_minute() << ":" << t.get_second();
+	return out;
+}
--- bank/bank/Date.h
+++ bank/bank/Date.h
@@ -1,16 +1,35 @@
 #pragma once
+#include <ctime>
+#include <iostream>
 
 using namespace std;
 
 class Date
 {
+private:
 	int _day;
 	int _month;
 	int _year;
 	int _hour;
 	int _minute;
 	int _second;
+public:
+	Date();
 
-	Date(int day, int month, int year, int hour, int minute, int second);
+	void set_day(int day) { _day = day; };
+	void set_month(int month) { _month = month; };
+	void set_year(int year) { _year = year; };
+	void set_hour(int hour) { _hour = hour; };
+	void set_minute(int minute) { _minute = minute; };
+	void set_second(int second) { _second = second; };
+
+	int get_day() { return _day; };
+	int get_month() { return _month; };
+	int get_year() { return _year; };
+	int get_hour() { return _hour; };
+	int get_minute() { return _minute; };
+	int get_second() { return _second; };
+
+	friend std::ostream& operator<<(ostream& out, Date& t);
 };
 
