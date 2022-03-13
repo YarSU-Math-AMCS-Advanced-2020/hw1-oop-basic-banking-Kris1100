5bc324e20af02333ad0fd7b4dc3bf7ac70b4deb4
Kris1100 <bobr.200@yandex.ru>
2022-03-13 12:50:33 +0300

    Change id_to_string and getter in DataBase

--- bank/bank/DataBase.h
+++ bank/bank/DataBase.h
@@ -16,9 +16,9 @@ private:
 	long long _max_id_account = 1; //Для аккаунта(не разделяем на персональный и не персональный)
 	long long _max_id_card = 1; //Для карты
 	long long _max_id_debit = 1; //Для дебетового счета
-	vector <BankAccount*> _base_account;
-	vector <DebitAccount*> _base_debit;
-	vector <DebitCard*> _base_card;
+	//vector <BankAccount*> _base_account;
+	//vector <DebitAccount*> _base_debit;
+	//vector <DebitCard*> _base_card;
 
 	DataBase() {};
 	DataBase& operator=(DataBase&);
@@ -40,9 +40,9 @@ public:
 
 
 
-	void add_account(BankAccount* account) { _base_account.push_back(account); };
-	void add_debit(DebitAccount* debit) { _base_debit.push_back(debit); };
-	void add_card(DebitCard* card) { _base_card.push_back(card); };
+	//void add_account(BankAccount* account) { _base_account.push_back(account); };
+	//void add_debit(DebitAccount* debit) { _base_debit.push_back(debit); };
+	//void add_card(DebitCard* card) { _base_card.push_back(card); };
 
 	void delete_card(long long card_id);
 	void delete_debit(long long debit_id);
