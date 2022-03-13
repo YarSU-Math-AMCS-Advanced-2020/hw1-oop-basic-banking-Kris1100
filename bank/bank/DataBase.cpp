fec7e007a80d8d8e59f5e3a8de2428d5f4993a70
Kris1100 <bobr.200@yandex.ru>
2022-03-13 12:13:59 +0300

    Transform DataBase in Singleton. Don't know if it works.

--- bank/bank/DataBase.cpp
+++ bank/bank/DataBase.cpp
@@ -1,1 +1,25 @@
 #include "DataBase.h"
+
+
+DataBase* DataBase::p_instance = 0;
+
+string DataBase::id_to_string(long long id) {
+	string s = to_string(id);
+	string id_str = "";
+	for (int i = 0; i < s.length(); i++)
+		id_str += "0";
+	id_str += s;
+	return id_str;
+}
+
+string DataBase::get_max_id_account() {
+	return(id_to_string(_max_id_account));
+}
+
+string DataBase::get_max_id_debit() {
+	return(id_to_string(_max_id_debit));
+}
+
+string DataBase::get_max_id_card() {
+	return(id_to_string(_max_id_card));
+}
--- bank/bank/DataBase.h
+++ bank/bank/DataBase.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <vector>
+#include <string>
 #include <iostream>
 #include "BankAccount.h"
 #include "DebitAccount.h"
@@ -9,18 +10,42 @@ using namespace std;
 //“еоретически Singleton, но что то € как то не знаю как это сделать
 class DataBase
 {
-protected:
+private:
+	static DataBase* p_instance;
 	//ƒл€ безопасности храним номер который будем использовать
 	long long _max_id_account = 1; //ƒл€ аккаунта(не раздел€ем на персональный и не персональный)
 	long long _max_id_card = 1; //ƒл€ карты
 	long long _max_id_debit = 1; //ƒл€ дебетового счета
-	vector <BankAccount&> _base_account;
-	vector <DebitAccount&> _base_debit;
-	vector <DebitCard&> _base_card;
-public:
-	void add_account(BankAccount& account) { _base_account.push_back(account); };
-	void add_debit(DebitAccount& debit) { _base_debit.push_back(debit); };
-	void add_card(DebitCard& card) { _base_card.push_back(card); };
+	vector <BankAccount*> _base_account;
+	vector <DebitAccount*> _base_debit;
+	vector <DebitCard*> _base_card;
 
+	DataBase() {};
+	DataBase& operator=(DataBase&);
+
+public:
+
+	static DataBase* getInstance() {
+		if (!p_instance)
+			p_instance = new DataBase();
+		return p_instance;
+	}
+
+	string id_to_string(long long id);
+
+	//будут возвращать строку длины 16, если длина числа меньше, запихиваем в начало нули
+	string get_max_id_account();
+	string get_max_id_debit();
+	string get_max_id_card();
+
+
+
+	void add_account(BankAccount* account) { _base_account.push_back(account); };
+	void add_debit(DebitAccount* debit) { _base_debit.push_back(debit); };
+	void add_card(DebitCard* card) { _base_card.push_back(card); };
+
+	void delete_card(long long card_id);
+	void delete_debit(long long debit_id);
 };
+
 
