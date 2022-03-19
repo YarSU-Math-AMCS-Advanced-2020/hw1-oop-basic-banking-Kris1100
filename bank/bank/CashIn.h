2d3a23e8ec58a693acdc062b2cc9022aea187665
Kris1100 <bobr.200@yandex.ru>
2022-03-19 16:04:23 +0300

    Add cash in and checks

--- bank/bank/CashIn.h
+++ bank/bank/CashIn.h
@@ -1,10 +1,19 @@
 #pragma once
+#include <iostream>
 #include "CashTransaction.h"
+#include "DebitAccount.h"
 
 using namespace std;
 
 class CashIn :
     public CashTransaction
 {
+public:
+    CashIn();
+    friend std::istream& operator>>(istream& in, CashIn& t);
+
 };
+
+
+
 
